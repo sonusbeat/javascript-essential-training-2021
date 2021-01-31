f31443399ae436478835349b33e5554fd638302c
Daniel Gonzalez Briseño <sonusbeat@hotmail.com>
2021-01-31 17:57:07 -0600

    Changing colors by Js

--- _snipppets/dom.js
+++ _snipppets/dom.js
@@ -1,5 +1,23 @@
-/*
-  Whipe out everything inside body or any
-  targeted element
-*/
-// document.body.innerHTML = "";
+/* ############### innerHTML ###############
+  Gets or sets the HTML or XML markup contained within the element.
+########################################### */
+// element.innerHTML;
+
+/* ############### outerHTML ###############
+  The outerHTML attribute of the Element DOM interface
+  gets the serialized HTML fragment describing the element
+  including its descendants.
+########################################### */
+// element.outerHTML;
+
+/*################# Query Selector #################
+  Select the target element, could be id, class or tag
+#################################################### */
+// querySelector("element");
+
+/* ############ Query Selector All ############
+  Select all the target elements,
+  and save them into array,
+  could be id, class or tag
+############################################## */
+// querySelectorAll("element");
--- lessons/05_02/script.js
+++ lessons/05_02/script.js
@@ -27,25 +27,27 @@ const content = `
     </figure>
     <h1 class="backpack__name">${everydayPack.name}</h1>
     <ul class="backpack__features">
-      <li class="backpack__volume">Volume:<span> ${
-        everydayPack.volume
-      }l</span></li>
+      <li class="backpack__volume">Volume:<span> ${everydayPack.volume}l</span></li>
       <li class="backpack__color">Color:<span> ${everydayPack.color}</span></li>
       <li class="backpack__age">Age:<span> ${everydayPack.backpackAge()} days old</span></li>
-      <li class="backpack__pockets">Number of pockets:<span> ${
-        everydayPack.pocketNum
-      }</span></li>
-      <li class="backpack__strap">Left strap length:<span> ${
-        everydayPack.strapLength.left
-      } inches</span></li>
-      <li class="backpack__strap">Right strap length:<span> ${
-        everydayPack.strapLength.right
-      } inches</span></li>
-      <li class="backpack__lid">Lid status:<span> ${
-        everydayPack.lidOpen
-      }</span></li>
+      <li class="backpack__pockets">Number of pockets:<span> ${everydayPack.pocketNum}</span></li>
+      <li class="backpack__strap">Left strap length:<span> ${everydayPack.strapLength.left} inches</span></li>
+      <li class="backpack__strap">Right strap length:<span> ${everydayPack.strapLength.right} inches</span></li>
+      <li class="backpack__lid">Lid status:<span> ${everydayPack.lidOpen}</span></li>
     </ul>
   </article>
 `;
 
 main.innerHTML = content;
+
+const list = document.querySelectorAll("main li");
+
+// for (let i = 0; i < list.length; i++) {
+//   list[i].style.color = "crimson";
+// }
+
+// list.forEach(function changeColor(item) {
+//   item.style.color = "crimson";
+// });
+
+list.forEach(item => (item.style.color = "crimson"));
