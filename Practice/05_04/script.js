b5250eb1ed5dbe81a864533c94dfd3111d57b96e
Daniel Gonzalez Briseño <sonusbeat@hotmail.com>
2021-01-31 18:18:54 -0600

    querySelector Excerscise

---
+++ Practice/05_04/script.js
@@ -0,0 +1,5 @@
+// 1- Identify specific elements you want to target
+// 2- Use the console (script.js) to find those target elements
+
+// const siteHeader = document.querySelector("header .site-title");
+// siteHeader.style.color = "blue";
