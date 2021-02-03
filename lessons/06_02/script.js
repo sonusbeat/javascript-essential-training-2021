041a9b239ae7768110c6fae62d3150b892ab879a
Daniel Gonzalez Briseño <sonusbeat@hotmail.com>
2021-02-02 19:30:10 -0600

    var Variable

--- lessons/06_02/script.js
+++ lessons/06_02/script.js
@@ -2,3 +2,18 @@
  * The var statement
  * @link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/var
  */
+var container = 5;
+
+console.log(container);
+
+container = "red";
+
+console.log(container);
+
+var x = 1,
+  y = 2,
+  z = 3;
+
+console.log("x:", x);
+console.log("y:", y);
+console.log("z:", z);
