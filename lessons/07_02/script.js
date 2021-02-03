50e35f6ec85ae36373afd08b03297b0cac5ef658
Daniel Gonzalez Briseño <sonusbeat@hotmail.com>
2021-02-02 20:27:28 -0600

    Array one o one

--- lessons/07_02/script.js
+++ lessons/07_02/script.js
@@ -8,3 +8,17 @@ let item = "flashlight";
 const collection = ["Piggy", item, 5, true];
 
 console.log(collection);
+
+console.log("lenght", collection.length);
+
+for (let i = 0; i < collection.length; i++) {
+  console.log(`[${i}]`, collection[i]);
+}
+
+collection[4] = "orange";
+
+console.log(collection);
+
+collection[0] = "cat";
+
+console.log(collection);
