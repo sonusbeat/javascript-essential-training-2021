331df4b42615717f91595f13c73d24c867d532e6
Daniel Gonzalez Briseño <sonusbeat@hotmail.com>
2021-02-02 19:52:15 -0600

    Data Type Comparisson

--- lessons/06_07/script.js
+++ lessons/06_07/script.js
@@ -3,13 +3,13 @@
  * @link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators#Relational_operators
  */
 
-let a = 5;
-let b = 4;
+let a = "5";
+let b = 5;
 
 console.log(`let a: ${a} (${typeof a})`);
 console.log(`let b: ${b} (${typeof b})`);
 
-if (a == b) {
+if (a === b) {
   console.log(`Match! let a and let b are the same value.`);
 } else {
   console.error(`No match: let a and let b are NOT same value.`);
