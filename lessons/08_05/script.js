c54b9eac662a86ba3c7620d9d9f785d0cc6f63f9
Daniel Gonzalez Briseño <sonusbeat@hotmail.com>
2021-02-03 21:45:14 -0600

    This in arrow functions

--- lessons/08_05/script.js
+++ lessons/08_05/script.js
@@ -4,22 +4,50 @@
  * @link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/this
  */
 
-// Define a default volume for the window (the entire DOM):
-window.volume = 20;
+window.price = 5.25;
 
-const greenPack = {
-  name: "Frog Pack",
-  color: "green",
-  volume: 8,
-  pocketNum: 3,
-  newVolume: function (volume) {
-    console.log("this.volume in the method:", this.volume);
-    this.volume = volume;
-    console.log("this.volume after update:", this.volume);
-    // (function () {
-    //   console.log("this.volume in nested function:", this.volume);
-    // })();
-  },
+const book = {
+  name: "Pinoccio",
+  price: 2.25,
+  quantity: 25,
+  state: "new",
+  active: true,
+
+  /**
+   * Set total price
+   * @param {number} tax Tax percentage
+   * @returns {number} Price with tax included
+   */
+  setTotal: function(tax) {
+    // (function(){
+    //   console.log("Product price:", this.price);
+    // })()
+
+    (() => console.log("Product price:", this.price))();
+    
+    return ((tax / 100) * this.price) + this.price;
+  }
 };
 
-console.log(greenPack.newVolume(5));
+
+console.log(book.setTotal(12));
+
+// Define a default volume for the window (the entire DOM):
+// window.volume = 20;
+
+// const greenPack = {
+//   name: "Frog Pack",
+//   color: "green",
+//   volume: 8,
+//   pocketNum: 3,
+//   newVolume: function (volume) {
+//     console.log("this.volume in the method:", this.volume);
+//     this.volume = volume;
+//     console.log("this.volume after update:", this.volume);
+//     // (function () {
+//     //   console.log("this.volume in nested function:", this.volume);
+//     // })();
+//   },
+// };
+
+// console.log(greenPack.newVolume(5));
